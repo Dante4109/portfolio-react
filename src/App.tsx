933f551164123dad715417cecd28aff9e17f4337
R3Z <rogerjohnmorellizeller@gmail.com>
2025-03-03 22:54:38 -0500

    comment out navbar for a moment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar/Navbar";
+/* import Navbar from "./components/Navbar/Navbar"; */
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
 import Services from "./components/Services/Services";
@@ -9,7 +9,7 @@ import Footer from "./components/Footer/Footer";
 const App = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <Hero />
       <About />
       <Services />
